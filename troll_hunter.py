c9ee4513f4b6508ed06fc94de0d379110ab979e8
Matt Duffy <duffyw99@users.noreply.github.com>
2018-01-28 10:51:10 -0500

    Started logging accounts in txt file
    Added functionality to log and append usernames to a txt file. Also exposed which subreddit negative comments are posted to in order to see if user is trolling particular forums. TODO: compare counts of neutral/positive posts to negative posts by subreddit forum to develop a ratio/score metric

--- troll_hunter.py
+++ troll_hunter.py
@@ -1,20 +1,27 @@
 import praw
+from admin_ignore.praw_keys import *  # clint_id, client_secret, pw, etc.
+import json
 import re
-from admin_ignore.praw_keys import * # clint_id, client_secret, pw, etc.
 import random
 import time
 
+# Establish connection to reddit
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent,
                      username=username,
                      password=password,
                      )
-subreddit = reddit.subreddit('all')
 
+karma_threshold = -10
+subreddit = reddit.subreddit('all')
 hot_submissions = subreddit.hot(limit=5)  # number of posts
 
-potential_trolls = []
+# Load known trolls
+with open('troll_registry.txt', 'r') as f:
+    potential_trolls = [line.strip() for line in f]
+
+# Scan comments from top posts for negitive comments
 for submission in hot_submissions:
     # print(dir(submission))
     submission_id = submission.id
@@ -22,14 +29,18 @@ for submission in hot_submissions:
 
     submission.comments.replace_more(limit=0)
     for comment in submission.comments:
-        if comment.score <= -10:
-            print(comment.author, comment.score, comment.downs) # , comment.body
-            if comment.author not in potential_trolls and comment.author != None:
+        if comment.score <= karma_threshold:
+            print(comment.author, comment.score, comment.downs)  # ,comment.body
+            if comment.author not in potential_trolls and comment.author is not None:
                 potential_trolls.append(comment.author)
-print(potential_trolls)
 
-for troll in potential_trolls:
-    trolls = reddit.redditor(str(troll))
-    for comment in trolls.comments.new(limit=None):
-        if comment.score < karma_threshold:
-            print(comment.author, comment.score, comment.body[:50])
+with open('troll_registry.txt', 'w') as file:
+    for troll in potential_trolls:
+        file.write("%s\n" % troll)
+        trolls = reddit.redditor(str(troll))
+        for comment in trolls.comments.new(limit=None):
+            if comment.score < karma_threshold:
+                print(comment.author,
+                      comment.subreddit,
+                      comment.score,
+                      comment.body[:50])
